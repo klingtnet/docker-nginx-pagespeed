3ad3eeb04e391f399580d24487e57a03daa80d97
Andreas Linz <anli@spreadshirt.net>
2015-05-26 10:12:38 +0200

    used config vars

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ HTTP_PORT=${HTTP_PORT:-8888}
 SSL_PORT=${SSL_PORT:-10043}
 SITES=${SITES:-$PWD/share/sites}
 CONFIG_PATH=${CONFIG_PATH:-$PWD/share/conf}
-docker run --rm --name ${NAME} -p 9999:80 -p 10443:443 -v ${CONFIG_PATH}:/etc/nginx -v ${SITES}:/usr/share/sites:ro nginx-pagespeed
+docker run --rm --name ${NAME} -p ${HTTP_PORT}:80 -p ${SSL_PORT}:443 -v ${CONFIG_PATH}:/etc/nginx -v ${SITES}:/usr/share/sites:ro nginx-pagespeed
